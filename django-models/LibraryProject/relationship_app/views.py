adf95762efa9d00be689c897ad9da36374175121
rotbaj <109310264+rotbaj@users.noreply.github.com>
2025-02-21 14:44:26 +0100

    django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,14 +1,17 @@
-from django.shortcuts import render
+# relationship_app/views.py
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
-from .models import Book  
-from .models import Library  
+from .models import Book
+from .models import Library
 
 def list_books(request):
-    """Function-based view to list all books."""
     books = Book.objects.all()
-    return render(request, "relationship_app/list_books.html", {"books": books})
+    return render(request, 'list_books.html', {'books': books})
 
-def library_detail(request, library_id):
-    """Function-based view to display details of a library."""
-    library = Library.objects.get(id=library_id)
-    return render(request, "relationship_app/library_detail.html", {"library": library})  
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'library_detail.html'
+    context_object_name = 'library'
+
+    def get_queryset(self):
+         return Library.objects.all()  # Important: Specify queryset
