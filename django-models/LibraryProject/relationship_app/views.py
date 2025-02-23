0dcc69ab9e59d54af61486c9eaf97de225791f37
rotbaj <109310264+rotbaj@users.noreply.github.com>
2025-02-23 15:45:44 +0100

    django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -63,19 +63,19 @@ def is_member(user):
     return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
 
 # Admin View
-@login_required
 @user_passes_test(is_admin)
+@login_required
 def admin_view(request):
-    return render(request, "admin_view.html")
+    return render(request, "relationship_app/admin_view.html")
 
 # Librarian View
-@login_required
 @user_passes_test(is_librarian)
+@login_required
 def librarian_view(request):
-    return render(request, "librarian_view.html")
+    return render(request, "relationship_app/librarian_view.html")
 
 # Member View
-@login_required
 @user_passes_test(is_member)
+@login_required
 def member_view(request):
-    return render(request, "member_view.html")
+    return render(request, "relationship_app/member_view.html")
