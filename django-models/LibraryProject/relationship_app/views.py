132a142bb3bc620bc94404ff7d6628bcddfe9d9d
rotbaj <109310264+rotbaj@users.noreply.github.com>
2025-02-23 16:00:44 +0100

    django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from .models import Book  
 from .models import Library
-from django.contrib.auth.decorators import login_required, user_passes_test, permission_required
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm  
