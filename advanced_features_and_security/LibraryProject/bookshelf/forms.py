89c2225ef6cd881a2928a2db8f25633d530a7254
rotbaj <109310264+rotbaj@users.noreply.github.com>
2025-03-01 11:51:27 +0100

    django

---
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Book
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'published_date']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter book title'}),
+            'author': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter author name'}),
+            'published_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+        }
