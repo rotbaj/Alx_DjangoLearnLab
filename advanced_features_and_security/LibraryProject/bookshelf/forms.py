785e97d65db2940d1e295dc0114bd889ee29559a
rotbaj <109310264+rotbaj@users.noreply.github.com>
2025-03-01 11:52:09 +0100

    django

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -10,3 +10,9 @@ class BookForm(forms.ModelForm):
             'author': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter author name'}),
             'published_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
         }
+
+# Adding ExampleForm (Generic form example)
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    message = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}))
