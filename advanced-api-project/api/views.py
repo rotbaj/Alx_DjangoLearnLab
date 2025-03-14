c4a80f4475fa66087a0a4a7f815bf1df7d4c820b
rotbaj <109310264+rotbaj@users.noreply.github.com>
2025-03-14 11:13:20 +0100

    advanced api task 3

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, permissions
+from django_filters import rest_framework
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticatedIsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
