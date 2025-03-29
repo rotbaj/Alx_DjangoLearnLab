f8eee1263ae06f5e399326d6c1dc83cfb1742791
olubaj <144109079+olubaj@users.noreply.github.com>
2025-03-29 16:59:06 +0100

    notifications app

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -9,4 +9,6 @@ router.register(r'comments', CommentViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('feed/', feed, name='feed'),
+    path('posts/<int:pk>/like/', LikePostView.as_view(), name='like-post'),
+    path('posts/<int:pk>/unlike/', UnlikePostView.as_view(), name='unlike-post'),
 ]
