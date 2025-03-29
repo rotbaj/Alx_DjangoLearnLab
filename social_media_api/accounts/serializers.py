9965767e36e2e89e94fcf53f37d12c63bd420995
olubaj <144109079+olubaj@users.noreply.github.com>
2025-03-29 15:13:14 +0100

    fixed serializer

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -5,52 +5,19 @@ from rest_framework.authtoken.models import Token
 User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
-    followers_count = serializers.SerializerMethodField()
-    
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers', 'followers_count']
-        extra_kwargs = {
-            'followers': {'read_only': True},
-        }
-    
-    def get_followers_count(self, obj):
-        return obj.followers.count()
+        fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers']
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(
-        write_only=True,
-        required=True,
-        style={'input_type': 'password'},
-        min_length=8,
-        error_messages={
-            'min_length': _('Password must be at least 8 characters long.')
-        }
-    )
-    password2 = serializers.CharField(
-        write_only=True,
-        required=True,
-        style={'input_type': 'password'},
-        label=_('Confirm Password')
-    )
-
+    password = serializers.CharField(write_only=True)
+    
     class Meta:
         model = User
-        fields = ['username', 'email', 'password', 'password2']
-        extra_kwargs = {
-            'email': {'required': True}
-        }
-    
-    def validate(self, data):
-        if data['password'] != data['password2']:
-            raise serializers.ValidationError(
-                {'password2': _('The two password fields must match.')}
-            )
-        return data
+        fields = ['username', 'email', 'password']
     
     def create(self, validated_data):
-        validated_data.pop('password2')
-        user = User.objects.create_user(
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password']
